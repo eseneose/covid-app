ef82d76a32e9451a4552059eb72cac1abd1c523c
eseneose <eseneose96@gmail.com>
2022-03-24 15:53:28 +0100

    Updates

---
+++ precache-manifest.2e658c746435bb5486d5dc1fb5d08cee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6aa4d1a59c4d0017b19f1d3616e2908",
+    "url": "/covid-app/index.html"
+  },
+  {
+    "revision": "a1efdb8fc40fb4056e72",
+    "url": "/covid-app/static/css/main.cd8ba9c3.chunk.css"
+  },
+  {
+    "revision": "50d9c698e1e064f9acbc",
+    "url": "/covid-app/static/js/2.f410a3a3.chunk.js"
+  },
+  {
+    "revision": "4ed2d4f00cf8324f872f01907affe619",
+    "url": "/covid-app/static/js/2.f410a3a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1efdb8fc40fb4056e72",
+    "url": "/covid-app/static/js/main.da0a6892.chunk.js"
+  },
+  {
+    "revision": "71c3b7904384ebf737c5",
+    "url": "/covid-app/static/js/runtime-main.0aa2c741.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-app/static/media/image.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea471a326bd2d6c0b757cfab632e7a4b.js"
+  "/covid-app/precache-manifest.2e658c746435bb5486d5dc1fb5d08cee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
